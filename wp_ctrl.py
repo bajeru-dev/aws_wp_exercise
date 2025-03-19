a47b169e86ba15f68f06faf791fcd69811607eb8
Dawid Bajerski <zombex@gmail.com>
2025-03-19 18:24:34 +0100

    add comments to code

--- wp_ctrl.py
+++ wp_ctrl.py
@@ -6,16 +6,19 @@ import requests
 
 class Wordpress:
     def __init__(self, url):
+        '''Get url and initialize arument parser '''
         self.url = url + '/wp-json/wp/v2/'
         self.init_parser()
 
     def login(self, username, password):
-        # self.auth = (username, password)
+        '''Digest credentials and prepare header'''
         credentials = username + ':' + password
         token = base64.b64encode(credentials.encode())
         self.header = {'Authorization': 'Basic ' + token.decode('utf-8')}
 
     def init_parser(self):
+        '''Initialize arguments'''
+        # Map of functions to commands and their arguments
         function_map = {
             'list_posts' : {'cmd': self.list_posts, 'args': []},
             'get_post' : {'cmd': self.get_post, 'args': ['post_id']},
@@ -25,14 +28,17 @@ class Wordpress:
         }
         parser = argparse.ArgumentParser()
         subparsers = parser.add_subparsers(dest='cmd', required=True)
+        # Loop over cmds and add parsers
         for name, info in function_map.items():
             info['parser'] = subparsers.add_parser(name)
             for argument in info['args']:
+                # Loop over args and add arguments
                 info['parser'].add_argument(argument, type=str)
             info['parser'].set_defaults(func=info['cmd'])
         self.args = parser.parse_args()
 
     def wp_request(self, method, request, **kwargs):
+        '''Wrapper function for requests.request'''
         response = requests.request(method, self.url + request, headers=self.header, json=kwargs.get('json'))
         if  response.status_code in [200, 201]:
             return response.json()
@@ -40,6 +46,7 @@ class Wordpress:
             raise Exception(f"Failed {method} to ({self.url + request}):\n{response.status_code} {response.reason}")
 
     def list_posts(self):
+        '''List all posts and their IDs'''
         posts = self.wp_request('GET', 'posts')
         print("id\tTitle")
         print('-'*30)
@@ -47,20 +54,24 @@ class Wordpress:
             print(f"{post['id']}\t{post['title']['rendered']}")
 
     def get_post(self):
+        '''Get post by ID'''
         post = self.wp_request('GET', 'posts'+ '/' + self.args.post_id)
         print(f"Title: {post['title']['rendered']}")
         print(f"Content: {post['content']['rendered']}\n")
 
+
     def create_post(self):
+        '''Create new post with title and content'''
         post_data = {
             'title': self.args.title,
             'content': self.args.content,
-            'status': 'publish'  # post status (draft, publish, etc.)
+            'status': 'publish'
         }
         self.wp_request('POST', 'posts', json=post_data)
         print("Post created successfully")
 
     def update_post(self):
+        '''Update post by ID, new title and new content'''
         post_data = {
             'title': self.args.title,
             'content': self.args.content
@@ -69,6 +80,7 @@ class Wordpress:
         print("Post updated successfully")
 
     def delete_post(self):
+        '''Delete post by ID'''
         self.wp_request('DELETE', f'posts/{self.args.post_id}')
         print("Post deleted successfully")
 
